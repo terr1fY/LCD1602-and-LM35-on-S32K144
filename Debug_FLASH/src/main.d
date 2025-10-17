670c8e0276d28100c680039b27b0a946b697e522
Vo Minh Luong <126398690+terr1fY@users.noreply.github.com>
2025-10-17 16:03:02 +0700

    Add files via upload

---
+++ Debug_FLASH/src/main.d
@@ -0,0 +1,61 @@
+src/main.o: ../src/main.c \
+ C:/EBS211_WORKSPACE2/LCD1602andLM35onS32K144/SDK/platform/devices/S32K144/include/S32K144.h \
+ C:/EBS211_WORKSPACE2/LCD1602andLM35onS32K144/board/peripherals_lpi2c_config_1.h \
+ C:/EBS211_WORKSPACE2/LCD1602andLM35onS32K144/board/lpi2c_driver.h \
+ C:/EBS211_WORKSPACE2/LCD1602andLM35onS32K144/SDK/platform/devices/device_registers.h \
+ C:/EBS211_WORKSPACE2/LCD1602andLM35onS32K144/SDK/platform/devices/common/s32_core_cm4.h \
+ C:/EBS211_WORKSPACE2/LCD1602andLM35onS32K144/SDK/platform/devices/S32K144/include/S32K144.h \
+ C:/EBS211_WORKSPACE2/LCD1602andLM35onS32K144/SDK/platform/devices/S32K144/include/S32K144_features.h \
+ C:/EBS211_WORKSPACE2/LCD1602andLM35onS32K144/SDK/platform/devices/devassert.h \
+ C:/EBS211_WORKSPACE2/LCD1602andLM35onS32K144/SDK/platform/devices/status.h \
+ C:/NXP/S32DS.3.5/S32DS/software/S32SDK_S32K1XX_RTM_4.0.1/platform/drivers/inc/edma_driver.h \
+ C:/EBS211_WORKSPACE2/LCD1602andLM35onS32K144/board/osif.h \
+ C:/EBS211_WORKSPACE2/LCD1602andLM35onS32K144/SDK/platform/devices/callbacks.h \
+ C:/EBS211_WORKSPACE2/LCD1602andLM35onS32K144/board/clock_config.h \
+ C:/EBS211_WORKSPACE2/LCD1602andLM35onS32K144/SDK/platform/drivers/inc/clock.h \
+ c:\ebs211_workspace2\lcd1602andlm35ons32k144\sdk\platform\drivers\src\clock\s32k1xx\clock_s32k1xx.h \
+ C:/EBS211_WORKSPACE2/LCD1602andLM35onS32K144/board/pin_mux.h \
+ C:/EBS211_WORKSPACE2/LCD1602andLM35onS32K144/SDK/platform/drivers/inc/pins_driver.h \
+ C:/EBS211_WORKSPACE2/LCD1602andLM35onS32K144/board/lpi2c_driver.h \
+ C:/EBS211_WORKSPACE2/LCD1602andLM35onS32K144/board/adc_driver.h \
+ C:/EBS211_WORKSPACE2/LCD1602andLM35onS32K144/board/osif.h
+
+C:/EBS211_WORKSPACE2/LCD1602andLM35onS32K144/SDK/platform/devices/S32K144/include/S32K144.h:
+
+C:/EBS211_WORKSPACE2/LCD1602andLM35onS32K144/board/peripherals_lpi2c_config_1.h:
+
+C:/EBS211_WORKSPACE2/LCD1602andLM35onS32K144/board/lpi2c_driver.h:
+
+C:/EBS211_WORKSPACE2/LCD1602andLM35onS32K144/SDK/platform/devices/device_registers.h:
+
+C:/EBS211_WORKSPACE2/LCD1602andLM35onS32K144/SDK/platform/devices/common/s32_core_cm4.h:
+
+C:/EBS211_WORKSPACE2/LCD1602andLM35onS32K144/SDK/platform/devices/S32K144/include/S32K144.h:
+
+C:/EBS211_WORKSPACE2/LCD1602andLM35onS32K144/SDK/platform/devices/S32K144/include/S32K144_features.h:
+
+C:/EBS211_WORKSPACE2/LCD1602andLM35onS32K144/SDK/platform/devices/devassert.h:
+
+C:/EBS211_WORKSPACE2/LCD1602andLM35onS32K144/SDK/platform/devices/status.h:
+
+C:/NXP/S32DS.3.5/S32DS/software/S32SDK_S32K1XX_RTM_4.0.1/platform/drivers/inc/edma_driver.h:
+
+C:/EBS211_WORKSPACE2/LCD1602andLM35onS32K144/board/osif.h:
+
+C:/EBS211_WORKSPACE2/LCD1602andLM35onS32K144/SDK/platform/devices/callbacks.h:
+
+C:/EBS211_WORKSPACE2/LCD1602andLM35onS32K144/board/clock_config.h:
+
+C:/EBS211_WORKSPACE2/LCD1602andLM35onS32K144/SDK/platform/drivers/inc/clock.h:
+
+c:\ebs211_workspace2\lcd1602andlm35ons32k144\sdk\platform\drivers\src\clock\s32k1xx\clock_s32k1xx.h:
+
+C:/EBS211_WORKSPACE2/LCD1602andLM35onS32K144/board/pin_mux.h:
+
+C:/EBS211_WORKSPACE2/LCD1602andLM35onS32K144/SDK/platform/drivers/inc/pins_driver.h:
+
+C:/EBS211_WORKSPACE2/LCD1602andLM35onS32K144/board/lpi2c_driver.h:
+
+C:/EBS211_WORKSPACE2/LCD1602andLM35onS32K144/board/adc_driver.h:
+
+C:/EBS211_WORKSPACE2/LCD1602andLM35onS32K144/board/osif.h:
